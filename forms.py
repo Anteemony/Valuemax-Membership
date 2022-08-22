1b0b3a541372fac085716ee55b103e5c65d48c49
Anteemony <>
2022-08-22 11:20:40 +0000

    created forms structure

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email
+
+class RegistrationForm(FlaskForm):
+  username = StringField("Username", validators=[DataRequired(), Length(min=2,max=20)])
+  email = StringField("Email", validators=[DataRequired(), Email()])
+  phone = StringField(validators=[DataRequired()])
+  submit = SubmitField("Add Member")
+
+class LoginForm(FlaskForm):
+  email = StringField("Email", validators=[Email()])
+  phone = StringField(validators=[DataRequired()])
+  password = PasswordField("Password", validators=[DataRequired()])
+  remember = BooleanField("Remember me")
+  submit = SubmitField("Add Member")
