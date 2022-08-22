8438ec6623108d3b9cdfe97651bdf16cb29640e1
Anteemony <>
2022-08-22 12:26:15 +0000

    added forms to templates

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 #i added a comment
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "0f3c34f7789f6917e12593945aa86bdb"
 
 @app.route("/")
 @app.route("/home")
@@ -9,12 +10,22 @@ def home():
   return render_template("home.html")
 
 @app.route("/login")
-def login():
-  return render_template("login.html", title="Login")
+def user_login():
+  return render_template("user_login.html", title="Login")
 
 @app.route("/admin")
+def admin_login():
+  return render_template("admin_login.html", title="Admin Login")
+
+#admin must be logged in for this route later on
+@app.route("/admin/home")
 def admin():
   return render_template("admin.html", title="Admin")
+
+#admin must be logged in for this route later on
+@app.route("/admin/register-member")
+def register():
+  return render_template("register.html", title="Register New Member")
   
 
 if __name__ == "__main__":
